d326b70ce10014e52de212f093c66c4488607a79
Polina <75391420+wezbicka@users.noreply.github.com>
2022-11-07 12:10:21 +0300

    Collected data into a structure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pprint import pprint
 
 from dotenv import load_dotenv
 from geopy import distance
@@ -26,18 +27,23 @@ def main():
     load_dotenv()
     apikey = os.environ['apikey']
     сoffeeshops = load_coffee_shops("coffee.json")
-    for num, cafe in enumerate(сoffeeshops):
-        cafe_coords = (cafe['geoData']['coordinates'][1],
-                       cafe['geoData']['coordinates'][0])
-        cafe_name = cafe['Name']
-
-    print(cafe_name, cafe_coords)
-
     location = input("Где вы находитесь? ")
     coords_point = fetch_coordinates.fetch_coordinates(apikey, location)
     latitude, longitude = change_coords(coords_point)
-    distance = find_distance(latitude, longitude, cafe_coords)
-    print(distance)
+    new_list_сoffeeshops = []
+    for num, cafe in enumerate(сoffeeshops):
+        cafe_coords = (
+            cafe['geoData']['coordinates'][1],
+            cafe['geoData']['coordinates'][0],
+        )
+        distance = find_distance(latitude, longitude, cafe_coords)
+        new_list_сoffeeshops.append({})
+        new_list_сoffeeshops[num]["title"] = cafe['Name']
+        new_list_сoffeeshops[num]["distance"] = distance
+        new_list_сoffeeshops[num]["latitude"] = cafe_coords[0]
+        new_list_сoffeeshops[num]["longitude"] = cafe_coords[1]
+        latitude, longitude = change_coords(coords_point)
+    pprint(new_list_сoffeeshops)
 
 
 if __name__ == '__main__':
